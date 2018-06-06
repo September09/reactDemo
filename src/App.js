595686b774884b60aa9ddd29ce85cc46e172ca78
Jiuyue Jia (贾九月) <jiuyuejia@administratordeiMac-2.local>
2018-06-06 17:54:05 +0800

    change style

--- src/App.js
+++ src/App.js
@@ -12,13 +12,9 @@ const Main = () => (
       <div><NavLink to="/home">Home</NavLink></div>
       <div><NavLink to="/stuff">Stuff</NavLink></div>
       <div><NavLink to="/contact">Contact</NavLink></div>
-    </div>
-
-    <div className="loginWrapper">
       <div><NavLink to="/login">登录</NavLink></div>
       <div><NavLink to="/register">注册</NavLink></div>
     </div>
-
   </div>
 );
 
--- src/containers/login.js
+++ src/containers/login.js
@@ -3,18 +3,30 @@
  */
 import React from 'react';
 import '../style/common.scss';
+import '../style/register.scss';
 
 const Login = () => (
   <div className="loginWrapper">
-    <div className="userWrapper">
-      <div className="col30 fl">用户名：</div>
-      <div className="col50 fl"><input type="text" placeholder="请输入用户名" /></div>
-    </div>
-    <div className="pwdWrapper">
-      <div className="col30 fl">密码：</div>
-      <div className="col50 fl"><input type="password" placeholder="请输入密码" /></div>
-    </div>
-    <button className="loginBtn">登录</button>
+    <div className="title">登录测试</div>
+    <form className="form-horizontal" id="loginForm">
+      <div className="form-group">
+        <label htmlFor="loginusername" className="col20 fl">用户名</label>
+        <div className="col80 fl">
+          <input type="text" className="inputStyle" id="loginusername" placeholder="请输入用户名" />
+        </div>
+      </div>
+      <div className="form-group">
+        <label htmlFor="loginpassword" className="col20 fl">密码</label>
+        <div className="col80 fl">
+          <input type="password" className="inputStyle" id="loginpassword" placeholder="请输入密码" />
+        </div>
+      </div>
+      <div className="form-group">
+        <div className="btnWrapper">
+          <button type="button" className="btn btn-default loginBtn" id="info_submit">登录</button>
+        </div>
+      </div>
+    </form>
   </div>
 );
 
